2919f2a1270a9d05f1bade89bc337365b3da4946
Erol Yarataner <eyarataner@gmail.com>
2022-02-15 20:27:53 +0100

    OOP was added

--- pvsimulator.py
+++ pvsimulator.py
@@ -7,9 +7,9 @@ import numpy as np
 exp_hour = np.array([0, 4, 8, 10, 12, 14, 16, 18, 19, 20, 21, 23])
 power_val = np.array([0, 0, 400, 1600, 2700, 3200, 3000, 2000, 1000, 200, 0, 0])
 
-# Power curve graphic based on hours
+''' Power curve graphic, based on hours
 plt.scatter(exp_hour, power_val)
-plt.show()
+plt.show()'''
 
 # Find the current hour to find out the power generation at that time
 now = datetime.datetime.now()
@@ -17,22 +17,38 @@ timestamp = now.strftime("On %A, %B %d, %Y at %H:%M o'clock")
 hour = now.hour
 
 # Random meter value - during the day (09:00 - 18:00) the energy consumption is supposed to be higher than night
-if 9 <= hour <= 18:
-    meter = random.randint(3000,9000)
-else:
-    meter = random.randint(0,2999)
+class Meter:
+    def home_consumption(self):
+        if 9 <= hour <= 18:
+            return random.randint(3000,9000)
+        else:
+            return random.randint(0,2999)
 
 # Interpolation of graphic, based on experimental hour and power values
 current_power = np.interp(hour, exp_hour, power_val).astype(int)
 
-if current_power > meter:
-    sell = current_power - meter
-    with open("simulator.txt", 'a') as f:
-        f.write(f"{timestamp} your PV production is {current_power}W and consumption is {meter}W - you can sell {sell}W to the grid\n")
-elif hour < meter:
-    buy = meter - current_power
-    with open("simulator.txt", 'a') as f:
-        f.write(f"{timestamp} your PV production is {current_power}W and consumption is {meter}W - you need {buy}W energy from the grid\n")
-else:
-    with open("simulator.txt", 'a') as f:
-        f.write(f"{timestamp} your PV production ({current_power}W) and consumption ({meter}W) are equal \n")
+# Simulator to calculate energy comparison
+class PVSimulator:
+    def __init__(self, meter_value):
+        self.meter_value = meter_value
+
+    def calculation(self):
+        if current_power > self.meter_value:
+            sell = current_power - self.meter_value
+            with open("simulator.txt", 'a') as f:
+                f.write(f"{timestamp} your PV production is {current_power}W and consumption is {self.meter_value}W - you can sell {sell}W to the grid\n")
+            return sell
+        elif current_power < self.meter_value:
+            buy = self.meter_value - current_power
+            with open("simulator.txt", 'a') as f:
+                f.write(f"{timestamp} your PV production is {current_power}W and consumption is {self.meter_value}W - you need {buy}W energy from the grid\n")
+            return buy
+        else:
+            with open("simulator.txt", 'a') as f:
+                f.write(f"{timestamp} your PV production ({current_power}W) and consumption ({self.meter_value}W) are equal \n")
+            return current_power
+
+
+if __name__ == '__main':
+    meter1 = Meter().home_consumption()
+    PVSimulator(meter1).calculation()
--- testss.py
+++ testss.py
@@ -1,9 +1,20 @@
 import random
-a  = random.randint(1,10)
+hour = 5
 
-if 3 < a < 7:
+class Meter:
+    def home_consumption(self):
+        if 9 <= hour <= 18:
+            return random.randint(3000,9000)
+        else:
+            return random.randint(0,2999)
+
+class Deneme:
+    def __init__(self, b):
+        self.b = b
+    def toplama(self):
+        return self.b + 5
+if __name__ == '__main__':
+    a = Meter().home_consumption()
+    cc = Deneme(a).toplama()
     print(a)
-    print('arada')
-else:
-    print(a)
-    print('derede')
+    print(cc)
